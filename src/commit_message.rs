9b39cb6f30b9a2fbef4eda3a88bf073636d5c250
Billie Thompson <billie@billiecodes.com>
2020-06-28 20:35:05 +0200

    Fix indentation

--- src/commit_message.rs
+++ src/commit_message.rs
@@ -614,20 +614,22 @@ impl CommitMessage {
     ///
     /// let commit = CommitMessage::from(indoc!(
     ///     "
-    ///        Example Commit Message
+    ///     Example Commit Message
     ///
-    ///        This is an example commit message for linting
-    ///        ; Bitte geben Sie eine Commit-Beschreibung f\u{00FC}r Ihre \u{00E4}nderungen ein. Zeilen,
-    ///        ; die mit ';' beginnen, werden ignoriert, und eine leere Beschreibung
-    ///        ; bricht den Commit ab.
-    ///        ;
-    ///        ; Auf Branch main
-    ///        ; Ihr Branch ist auf demselben Stand wie 'origin/main'.
-    ///        ;
-    ///        ; Zum Commit vorgemerkte \u{00E4}nderungen:
-    ///        ;    neue Datei:     file
-    ///        ;
-    ///        "
+    ///     This is an example commit message for linting
+    ///
+    ///
+    ///     ; Bitte geben Sie eine Commit-Beschreibung f\u{00FC}r Ihre \u{00E4}nderungen ein. Zeilen,
+    ///     ; die mit ';' beginnen, werden ignoriert, und eine leere Beschreibung
+    ///     ; bricht den Commit ab.
+    ///     ;
+    ///     ; Auf Branch main
+    ///     ; Ihr Branch ist auf demselben Stand wie 'origin/main'.
+    ///     ;
+    ///     ; Zum Commit vorgemerkte \u{00E4}nderungen:
+    ///     ;    neue Datei:     file
+    ///     ;
+    ///     "
     /// ));
     ///
     /// let re = Regex::new("[Bb]itte").unwrap();
